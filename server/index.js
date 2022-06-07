25355fc1dced2590fe1e8e1ad2c6c5a3cc71541d
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-06-07 21:17:12 +1000

    feat: modify package.json

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ connectDB();
 app.use(cors());
 
 __dirname = path.resolve();
-if (process.env.NODE_ENV === 'prod') {
+if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '/client/build')));
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
