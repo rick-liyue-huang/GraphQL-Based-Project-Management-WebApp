bc31a04bb8333e27d9b248e76f7438dd01391d71
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-06-07 10:28:08 +1000

    feat: add mongoose model type project and client

---
+++ server/models/Project.js
@@ -0,0 +1,22 @@
+
+const mongoose = require('mongoose');
+
+const ProjectSchema = new mongoose.Schema({
+	name: {
+		type: String
+	},
+	description: {
+		type: String
+	},
+	status: {
+		type: String,
+		enum: ['In Progress', 'Done', 'Not Started']
+	},
+	clientId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Client'
+	}
+});
+
+
+module.exports = mongoose.model('Project', ProjectSchema);
